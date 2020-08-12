cffa879c88384c79e49d98a43b5c697a26f07948
nagmnt <takahiro.ngmn@gmail.com>
2020-08-12 20:21:42 +0900

    fixed

--- capstone.py
+++ capstone.py
@@ -1,13 +1,14 @@
 from scipy.stats import mvn
 import numpy as np
+import math
 
-def knock_in_barrier(h_x, h_y, m_x, m_y, rho):
+def knock_in_barrier(h_x, h_y, m_x, m_y, vol_x, vol_y, rho):
     INF = 100000
     m = np.array([m_x, m_y]) #means
-    s = np.array([[1, rho],[rho, 1]]) #variance-covariance
+    s = np.array([[vol_x, rho*math.sqrt(vol_x)*math.sqrt(vol_y)],[rho*math.sqrt(vol_x)*math.sqrt(vol_y), vol_y]]) #variance-covariance
     low = np.array([h_x, -1 * INF]) #integral lower
     upp = np.array([INF, h_y]) #integral upper
     p, i = mvn.mvnun(low, upp, m, s)
     return p
 
-print(knock_in_barrier(0, 0, 0, 0, 0))
+print(knock_in_barrier(0, 0, 0, 0, 1, 1, 0))
